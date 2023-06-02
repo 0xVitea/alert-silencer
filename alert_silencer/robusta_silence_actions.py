576ed9cb17c13b85de187fa514787677d5d67e7f
Victor Rusnac <victor.rusnac20@gmail.com>
2023-06-02 11:39:19 +0100

    v1.8

--- alert_silencer/robusta_silence_actions.py
+++ alert_silencer/robusta_silence_actions.py
@@ -1,4 +1,5 @@
 import logging
+from typing import Any, Dict, List
 
 from robusta.api import (ActionParams, CallbackBlock, CallbackChoice,
                          ExecutionBaseEvent, PrometheusKubernetesAlert, action)
@@ -8,40 +9,53 @@ class AlertManagerURL(ActionParams):
     """
     :var alert_manager_url: Alert Manager url
     """
+
     alert_manager_url: str
 
 
 class AlertManagerParams(AlertManagerURL):
     """
-    :var alert_label: Alert Name
+    :var alert_labels: Alert Name
+    :var silence_interval: Interval for silencing in hours
     """
-    alert_label: str
+
+    alert_labels: Dict[Any, Any]
+    silence_interval: int
 
 
+# silencer - enricher callback function
 @action
-def sampler(event: ExecutionBaseEvent, params: AlertManagerParams):
-    logging.info(params.alert_label)
+def silencer(event: ExecutionBaseEvent, params: AlertManagerParams):
+    logging.info(params.alert_labels)
     logging.info(params.alert_manager_url)
-    print(100000*"*")
+    logging.info(params.silence_interval)
+    print(100 * "*")
 
 
+# silence_enricher - main silence enrichment function
 @action
 def silence_enricher(alert: PrometheusKubernetesAlert, params: AlertManagerURL):
     """
     Add a button to the alert - clicking it to silence the alert
     """
-    alert.add_enrichment(
-        [
-            CallbackBlock(
-                {
-                    "Silence for 15 minutes": CallbackChoice(
-                        action=sampler,
-                        action_params=AlertManagerParams(
-                            alert_label=alert.alert.labels.get("alertname", ""),
-                            alert_manager_url=params.alert_manager_url,
-                        ),
-                    )
-                },
-            )
-        ]
-    )
+
+    # List of silence intervals in hours
+    silence_intervals_hours: List[int] = [1, 4, 24]
+
+    for interval in silence_intervals_hours:
+        alert.add_enrichment(
+            [
+                CallbackBlock(
+                    {
+                        f"Silence for {interval}h": CallbackChoice(
+                            action=silencer,
+                            action_params=AlertManagerParams(
+                                alert_labels=alert.alert.labels,
+                                alert_manager_url=params.alert_manager_url,
+                                silence_interval=interval,
+                            ),
+                        )
+                    },
+                )
+            ]
+        )
